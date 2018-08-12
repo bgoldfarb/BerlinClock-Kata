4d549bc9c08571134604101791eb8cc56248b419
Bgoldfa <bgoldfa@gap.com>
2018-08-12 13:27:09 -0400

    Functional approach to berlin minutes

--- src/berlin-clock-minutes.js
+++ src/berlin-clock-minutes.js
@@ -1,22 +1,14 @@
 
-export default (timeString) => {
+import {fill} from 'lodash'
 
+
+export default (timeString) => {
     let convertedToIntTime = parseInt(timeString)
     let remainder = convertedToIntTime % 5
-    let timeArray = []
-    for(let i = 0; i < 4; i++){
-        if(remainder > 0){
-            timeArray.push('Y')
-        }
-        else{
-            timeArray.push('O')
-        }
-        remainder--
-    }
-   return timeArray.join('')
+    let timeArray = Array(4)
+    // for(let i = 0; i < 4; i++){
+    //     remainder-- > 0 ?  timeArray.push('Y') :  timeArray.push('O')
+    // }
+    let funcArray = timeArray.fill().map(() => (remainder-- > 0 ? 'Y' : 'O'))
+    return funcArray.join('')
 }
-
-
-
-
-
